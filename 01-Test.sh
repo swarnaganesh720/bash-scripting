5e4a271cd65d3d63e63cb8f83336eb251278314e
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-07-15 18:59:39 +0530

    Sample code commited

--- 01-Test.sh
+++ 01-Test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Test file to check the out put
-yum install httpd -y
+
+# First line should always be SHEBANG Notation and should be only one SHEBANG Notation apart from that # will be referred as comment
+echo "Hello World"
+
+## Printinhg Multiple lines
+
+echo "Hai"
+echo "Hello"
+
+## \n : New LIne ;  ## \t : Tab Space 
+
+echo -e "Hai\n\tHello"
