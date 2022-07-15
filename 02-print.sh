c53eb0386abb391bc0d8bfd833005b537e56e50c
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-07-15 20:33:35 +0530

    Sample background colour commit

--- 02-print.sh
+++ 02-print.sh
@@ -1,9 +1,15 @@
 #!/bin/bash 
 
-
+## To have a fourground colour
 # echo -e "\e[COL-CODEm  MESSAGE  \e[0m" 
 
 echo -e "\e[32m hi BOSS\e[0m"
 echo -e "\e[33m Hi BOSS\e[0m"
 echo -e "\e[43m HI BOSS \e[0m"
 echo -e "\e[48m Hi BOBY \e[0m"
+
+## To have a background color
+# echo -e "\e[background; COL-CODEm  MESSAGE  \e[0m" 
+
+echo -e "\e[45;33m I am printing COLORS \e[0m"
+echo -e "\e[47;38m Hi Jorge \e[0m"
