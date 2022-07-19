a962b21553b24e29ca1f7c692b62290cb9f042f6
Swarna Ganesh <swarna.ganesh@lntecc.com>
2022-07-19 22:54:16 +0530

    create ec2 instances

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -2,5 +2,5 @@
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
- echo "$COMPONENT Server Creation in progress"
- aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+echo "$COMPONENT Server Creation in progress"
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
